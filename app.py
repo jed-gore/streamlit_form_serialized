1269a8a7e555aaf10d1a460309d9bd6c384ef424
Jed Gore <jedgore@Jeds-Mac-mini.local>
2023-04-06 17:52:07 -0400

    update for one to many

--- app.py
+++ app.py
@@ -13,6 +13,16 @@ from sqlalchemy.orm import (
 )
 
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field
+from marshmallow_sqlalchemy.fields import Nested
+
+
+class SmartNested(Nested):
+    def serialize(self, attr, obj, accessor=None):
+        if attr not in obj.__dict__:
+            return {"id": int(getattr(obj, attr + "_id"))}
+        return super(SmartNested, self).serialize(attr, obj, accessor)
+
 
 # create local sqlite db
 engine = sa.create_engine("sqlite:///data.db", echo=True)
@@ -31,12 +41,19 @@ class User(Base):
         return "<User(name={self.name!r})>".format(self=self)
 
 
+class UserObject(Base):
+    __tablename__ = "user_objects"
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey("users.id"))
+    user = relationship("User", backref=backref("users"))
+
+
 # create tables ( strange error if already exists - so this is caught in a try except )
 try:
     Base.metadata.create_all(engine)
 except:
     print("database already created")
-
 
 # generate marshmallow schemas
 
@@ -48,6 +65,15 @@ class UserSchema(SQLAlchemyAutoSchema):
         load_instance = True
 
 
+class UserObjectSchema(SQLAlchemySchema):
+    id = auto_field()
+    user = SmartNested(UserSchema)
+
+    class Meta:
+        model = UserObject
+        sqla_session = session
+
+
 # create simple streamlit form
 
 
@@ -55,27 +81,27 @@ def form():
     st.write("Form Template")
     with st.form(key="Information form"):
         name = st.text_input("Enter user name: ")
-
+        object_name = st.text_input("Enter user object: ")
         submission = st.form_submit_button(label="Submit")
         if submission == True:
-            add_data(name)
+            add_data(name, object_name)
 
 
 # add data function drives the SQL insert and commit
 
 
-def add_data(name):
-    user = User(name=name)
-    user_schema = UserSchema()
-    session.add(user)
+def add_data(name, object_name):
+    user_object = UserObject(id=1)
+    user_object.name = object_name
+    user_object.user = User(name=name)
+    session.add(user_object)
     session.commit()
     st.success("Saved")
-    dump_data = user_schema.dump(user)
-    st.markdown(dump_data)  # dump the serialized json to the screen
+
+    # st.markdown(dump_data)  # dump the serialized json to the screen
 
     return
 
 
-# run the form
 if __name__ == "__main__":
     form()
